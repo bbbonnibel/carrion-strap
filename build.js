a7d0848f796d5e14f12246f8ed74172e079434c6
Bonnibel <4585677+scarletcs@users.noreply.github.com>
2026-01-15 16:38:23 +0000

    add chat themes

--- build.js
+++ build.js
@@ -6,21 +6,32 @@ const PATH_RELEASE = path.join(__dirname, "release");
 const PATH_SRC = path.join(__dirname, "src");
 fs.emptyDirSync(PATH_RELEASE);
 
-const files = [["main.scss", "main.css"]];
+const files = [
+  { source: "main.scss", out: "main.css" },
+  { source: "chat/theme.scss", out: "chat/theme.css" },
+  // { source: "chat/themes/reset.scss", out: "chat/themes/reset.css" },
+];
 
-for (const [source, out] of files) {
-  const filepath = path.join(PATH_SRC, source);
-  if (!fs.existsSync(filepath)) {
+for (const config of files) {
+  const { source, out } = config;
+
+  const sourcePath = path.join(PATH_SRC, source);
+
+  if (!fs.existsSync(sourcePath)) {
     console.error(`Cannot build this file because it doesn't exist:`, source);
     break;
   }
   let style;
   try {
-    style = sass.compile(filepath);
+    style = sass.compile(sourcePath);
   } catch (ex) {
     console.error(`Failed to build "${source}". Reason:`, ex);
     break;
   }
-  fs.writeFileSync(path.join(PATH_RELEASE, out), style.css);
+
+  const outPath = path.join(PATH_RELEASE, out);
+  const outFolder = path.join(outPath, "..");
+  fs.ensureDirSync(outFolder);
+  fs.writeFileSync(outPath, style.css);
   console.log("Created:", out);
 }
